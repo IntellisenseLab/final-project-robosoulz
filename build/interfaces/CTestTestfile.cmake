0efffb7d1c7ffda2ee8ff84e0d9ce8511499cd21
birajithk <birajithk@gmail.com>
2026-03-31 13:07:43 +0530

    Remove generated ROS workspace folders from repository

--- build/interfaces/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/birajith/Documents/Robotics/final-project-robosoulz/src/interfaces
-# Build directory: /home/birajith/Documents/Robotics/final-project-robosoulz/build/interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("interfaces__py")
